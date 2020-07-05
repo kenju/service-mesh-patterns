e4124b07c736928e5db275214569cd4637bf80d5
kenju <ken901waga@gmail.com>
2020-07-05 16:10:01 +0900

    remove unused files

--- k8s-fluentd-logging-kibana/api/app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::API
-  def route_not_found
-    render 'error_pages/404', status: :not_found
-  end
-end
--- k8s-fluentd-logging-kibana/api/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  get 'health', action: :show, controller: 'health'
-  get 'error', action: :error, controller: 'health'
-  get '*unmatched_route', to: 'application#route_not_found'
-end
